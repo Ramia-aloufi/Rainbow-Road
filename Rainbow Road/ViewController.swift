b0abb92601680a253eb73773174404e2267bceae
RAM111111 <r.r.99@windowslive.com>
2021-12-08 20:16:21 +0300

    first commit

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let color = [UIColor.red,UIColor.orange,UIColor.yellow,UIColor.green,UIColor.blue,UIColor.purple]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hi")!
+        cell.backgroundColor = color[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.bounds.height / 6
+    }
+    
+    
 }
 
